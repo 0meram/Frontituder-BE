592447162994e13fa2d3828b9223486bf791346c
omer ram <0meram95@gmail.com>
2021-11-25 19:52:41 +0200

    getCity

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const lngDetector = new (require("languagedetect"))();
 const router = express.Router();
-lngDetector.setLanguageType("iso2");
 
 router.post("/send", async (req, res) => {
 	try {
@@ -12,5 +11,57 @@ router.post("/send", async (req, res) => {
 		res(err);
 	}
 });
+
+router.get("/getCity", async (req, res) => {
+    console.log('~ req', req.body);
+	try {
+			let res = "";
+			const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+			const query = `?apikey=UIR2sXAbp5TQZRAuMOwP2o89JPkd9aHr&q=${city}`;
+			await axios.get(base + query).then((response) => {
+				if (response.data.length !== 0) {
+					return (res = response.data[0].Key);
+				} else {
+					return (res = "error");
+				}
+			});
+			res.send(res);
+	} catch (err) {
+		console.log(err);
+		res(err);
+	}
+});
+
+
+// export const getCity = async (city) => {
+
+// 	let res = "";
+// 	const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+// 	const query = `?apikey=UIR2sXAbp5TQZRAuMOwP2o89JPkd9aHr&q=${city}`;
+// 	await axios.get(base + query).then((response) => {
+// 		if (response.data.length !== 0) {
+// 			return (res = response.data[0].Key);
+// 		} else {
+// 			return (res = "error");
+// 		}
+// 	});
+// 	return res;
+// };
+
+// export const getForcast = async (id) => {
+// 	let res = "";
+// 	const base = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/`;
+// 	const query = `${id}?apikey=UIR2sXAbp5TQZRAuMOwP2o89JPkd9aHr`;
+// 	await axios.get(base + query).then((response) => {
+// 		res = response;
+// 		if (response.data.DailyForecasts.length !== 0) {
+// 			return (res = response.data.DailyForecasts);
+// 		} else {
+// 			return (res = "error");
+// 		}
+// 	});
+// 	return res;
+// };
+
 
 module.exports = router;
