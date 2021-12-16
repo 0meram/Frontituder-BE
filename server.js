854b0c5c36e0512e98146aeb1c3b45311aeebee9
omer ram <0meram95@gmail.com>
2021-12-16 11:58:46 +0200

    http

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = "https://herolo-be.herokuapp.com/";
 const cors = require("cors");
 
 app.use(express.json());
